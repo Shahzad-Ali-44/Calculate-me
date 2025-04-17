de4c0bf8f74db1a754d78d3904e26c2581c8c804
Shahzad Ali <shahzad.ali41410@gmail.com>
2025-04-17 13:10:11 +0500

    update ui

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-
 let string = "";
 let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach((button)=>{
-  button.addEventListener('click', (e)=>{
-    if(e.target.innerHTML == '='){
-      string = eval(string);
-      document.querySelector('input').value = string;
-    }
-    else if(e.target.innerHTML == 'C'){
-      string = ""
-      document.querySelector('input').value = string;
-    }
-    else{ 
-    string = string + e.target.innerHTML;
-    document.querySelector('input').value = string;
+Array.from(buttons).forEach((button) => {
+  button.addEventListener('click', (e) => {
+    if (e.target.innerHTML == '=') {
+      try {
+        string = eval(string).toString();  
+        document.querySelector('input').value = string;
+      } catch (error) {
+        document.querySelector('input').value = 'Error';
+        string = '';
       }
-  })
-})
+    }
+    else if (e.target.innerHTML == 'C') {
+      string = "";
+      document.querySelector('input').value = string;
+    }
+    else {
+      string += e.target.innerHTML;
+      document.querySelector('input').value = string;
+    }
+  });
+});
